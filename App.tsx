348ed06781562bdce4dafa08491f62962018ab0f
Angel <salazar.angel.e@gmail.com>
2022-03-17 18:34:36 -0300

    modificacion del home con paper

--- App.tsx
+++ App.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import { SafeAreaView, Text, View } from "react-native";
+import { Appbar } from 'react-native-paper';
 
 
 const App = () => {
+
+  const _goBack = () => console.log('Went back');
+
+  const _handleSearch = () => console.log('Searching');
+
+  const _handleMore = () => console.log('Shown more');
+
   return (
     <SafeAreaView>
-        <Text>Hola App</Text>
+        <Appbar.Header>
+          <Appbar.BackAction onPress={_goBack} />
+          <Appbar.Content title="Title" subtitle="Subtitle" />
+          <Appbar.Action icon="magnify" onPress={_handleSearch} />
+          <Appbar.Action icon="dots-vertical" onPress={_handleMore} />
+        </Appbar.Header>
     </SafeAreaView>
   )
 };
