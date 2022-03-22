cff67ada610b3d571feccb3063c1c01613ab0e03
Angel <salazar.angel.e@gmail.com>
2022-03-22 16:37:14 -0300

    se agrega el boton de login de google y configuracion con android

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, StyleSheet, TouchableOpacity, Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { Text } from 'react-native-paper';
@@ -10,6 +10,12 @@ import { TextInput } from '../components/TextInput'
 import { stylesLogin } from '../theme/loginTheme';
 import { theme } from '../theme/theme';
 
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from '@react-native-google-signin/google-signin';
+
 
 export const LoginScreen = () => {
 
@@ -18,9 +24,41 @@ export const LoginScreen = () => {
   const [email, setEmail] = useState({ value: '', error: '' })
   const [password, setPassword] = useState({ value: '', error: '' })
 
+  const [loggedIn, setloggedIn] = useState(false);
+  const [userInfo, setuserInfo] = useState([]);
+
 const onLoginPressed = () => {
   navigator.navigate('HomeScreen');
+}
+
+const signIn = async () => {
+  try {
+    await GoogleSignin.hasPlayServices();
+    const userInfo = await GoogleSignin.signIn();
+    setloggedIn(true)
+  } catch (error) {
+    // if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+    //   // user cancelled the login flow
+    // } else if (error.code === statusCodes.IN_PROGRESS) {
+    //   // operation (e.g. sign in) is in progress already
+    // } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+    //   // play services not available or outdated
+    // } else {
+    //   // some other error happened
+    // }
+    console.log(error)
   }
+};
+
+
+useEffect(() => {
+  GoogleSignin.configure({
+    scopes: ['email'], // what API you want to access on behalf of the user, default is email and profile
+    webClientId:
+      '418977770929-g9ou7r9eva1u78a3anassxxxxxxx.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
+    offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+  });
+}, []);
 
 
   return (
@@ -56,6 +94,14 @@ const onLoginPressed = () => {
           <Button mode="contained" onPress={onLoginPressed}>
             Login
           </Button>
+
+
+          <GoogleSigninButton
+                style={{width: 192, height: 48}}
+                size={GoogleSigninButton.Size.Wide}
+                color={GoogleSigninButton.Color.Dark}
+                onPress={signIn}
+              />
           <View style={ stylesLogin.row }>
             <Text>Don’t have an account? </Text>
             <TouchableOpacity onPress={() => navigator.replace('RegisterScreen')}>
