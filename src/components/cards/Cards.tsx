ab2e35757bd26a9d2fca89bb6586c16998777a02
Edgardo Hoyle <edgardo.hoyle@globant.com>
2022-03-24 15:47:44 -0300

    styles added

--- src/components/cards/Cards.tsx
+++ src/components/cards/Cards.tsx
@@ -1,17 +1,18 @@
 import React, {FC} from 'react';
 import { TouchableOpacity } from 'react-native-gesture-handler'
-import {Avatar, Card, Divider} from 'react-native-paper';
+import {Avatar, Card} from 'react-native-paper';
+import { stylesCard } from '../../theme/cardTheme'
 import {CardsProps} from '../../types/types';
 
 const Cards: FC<CardsProps> = props => {
   return (
     <TouchableOpacity onPress={props.onPress}>
       <Card.Title
+        style= {stylesCard.container}
         title={props.title}
         subtitle={`Your expense were: ${props.subtitle}`}
-        left={() => <Avatar.Icon size={48} icon={props.icon} />}
+        left={() => <Avatar.Icon style= {stylesCard.logo} size={48} icon={props.icon} />}
       />
-      <Divider />
     </TouchableOpacity>
   );
 };
