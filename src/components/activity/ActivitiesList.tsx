5d1948f3d911ab8a75fdfbc16fee0bca11aad20f
Angel <salazar.angel.e@gmail.com>
2022-03-29 18:31:18 -0300

    se agrega infinite scroll

--- src/components/activity/ActivitiesList.tsx
+++ src/components/activity/ActivitiesList.tsx
@@ -1,8 +1,11 @@
+/* eslint-disable prettier/prettier */
 import {useNavigation} from '@react-navigation/native';
 import React, {useEffect, useState} from 'react';
+import { View } from 'react-native';
 import {FlatList} from 'react-native-gesture-handler';
+import { ActivityIndicator } from 'react-native-paper';
 import {SafeAreaView} from 'react-native-safe-area-context';
-import {getExpenses} from '../../services/services';
+import {getExpenses, getMoreExpenses } from '../../services/services';
 import Cards from '../cards/Cards';
 
 type Nav = {
@@ -10,14 +13,47 @@ type Nav = {
 };
 
 const ActivitiesList = props => {
-  const [expenses, setExpenses] = useState([]);
+  const [expenses, setExpenses] = useState<any>([]);
+
+  const [startAfter, setStartAfter] = useState(Object);
+
+  const [postPerPage] = useState(20);
+
+  const [lastPost, setLastPost] = useState(false);
 
   const navigation = useNavigation<Nav>();
 
   useEffect(() => {
-    getExpenses({setExpenses});
+    getDataExpenses();
   }, []);
 
+
+ async function getDataExpenses() {
+   const allExpenses = await getExpenses(postPerPage);
+   setExpenses([...expenses, ...allExpenses.expenses]);
+   setStartAfter(allExpenses.lastItem);
+ }
+
+ async function getMoreDataExpenses() {
+   if (!lastPost){
+      const allExpenses = await getMoreExpenses(startAfter, postPerPage);
+      setExpenses([...expenses, ...allExpenses.expenses]);
+      setStartAfter(allExpenses.lastItem);
+      allExpenses.expenses.length === 0 ? setLastPost(true) :  setLastPost(false);
+   }
+  
+}
+
+const loader = () => {
+  return (
+    !lastPost ?
+    <View>
+      <ActivityIndicator color='red' size={50} />
+    </View> : null
+  );
+};
+
+  
   function renderItem({item}) {
     return (
       <Cards
@@ -35,6 +71,9 @@ const ActivitiesList = props => {
         data={expenses}
         keyExtractor={item => item.key}
         renderItem={renderItem}
+        onEndReached={getMoreDataExpenses}
+        onEndReachedThreshold={0.5}
+        ListFooterComponent={loader}
       />
     </SafeAreaView>
   );
