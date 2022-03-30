6a5eb6c567165c8a94a5a6e6f2a5eb7f94401248
Angel <salazar.angel.e@gmail.com>
2022-03-30 09:48:24 -0300

    se instala asyncStorage

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,10 +1,11 @@
 /* eslint-disable prettier/prettier */
- import React, { createContext, useReducer } from 'react';
+ import React, { createContext, useEffect, useReducer } from 'react';
  import auth from '@react-native-firebase/auth';
 import { LoginData, RegisterData, Usuariox } from '../interfaces/appInterfaces';
 import { authReducer, AuthState } from './authReducer';
 
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
  type AuthContextProps = {
     user: Usuariox | null;
@@ -29,6 +30,32 @@ export const AuthContext = createContext({} as AuthContextProps);
 export const AuthProvider = ({ children }: any) => {
     
     const [ state, dispatch ] = useReducer( authReducer, authInicialState);
+
+    useEffect(() => {
+        checkToken();
+    }, [])
+
+    const checkToken = async() => {
+        const token = await AsyncStorage.getItem('token');
+        
+        // No token, no autenticado
+        if ( !token ) return dispatch({ type: 'notAuthenticated' });
+
+        // Hay token
+       /* const resp = await cafeApi.get('/auth');
+        if ( resp.status !== 200 ) {
+            return dispatch({ type: 'notAuthenticated' });
+        }
+        
+        await AsyncStorage.setItem('token', resp.data.token );
+        dispatch({ 
+            type: 'signUp',
+            payload: {
+                token: resp.data.token,
+                user: resp.data.usuario
+            }
+        });*/
+    }
 
     const signIn = async ( {email, password }: LoginData ) => {
         try{
