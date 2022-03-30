c40780a60315307e72c61424284e623a9af16978
Edgardo Hoyle <edgardo.hoyle@globant.com>
2022-03-30 09:26:11 -0300

    home screen and styles added

---
+++ src/theme/homeTheme.tsx
@@ -0,0 +1,51 @@
+import {StyleSheet} from 'react-native';
+
+export const stylesHome = StyleSheet.create({
+  avatar: {
+    backgroundColor: '#4682B4',
+  },
+  balance: {
+    color: '#4682B4',
+    fontSize: 25,
+  },
+  btn: {
+    backgroundColor: '#4682B4',
+    marginTop: 40,
+    width: 200,
+  },
+  container: {
+    alignItems: 'center',
+    flex: 1,
+  },
+  subtitle: {
+    color: '#4682B4',
+    fontSize: 35,
+    fontWeight: '600',
+    marginBottom: 20,
+  },
+  surface: {
+    alignItems: 'center',
+    borderRadius: 10,
+    elevation: 5,
+    flexDirection: 'row',
+    height: 50,
+    justifyContent: 'space-between',
+    margin: 5,
+    paddingHorizontal: 20,
+    width: 380,
+  },
+  text: {
+    color: '#4682B4',
+    fontSize: 35,
+    fontWeight: '400',
+    marginTop: 20,
+  },
+  title: {
+    color: '#4682B4',
+    fontSize: 35,
+    fontStyle: 'italic',
+    fontWeight: '400',
+    margin: 40,
+    textTransform: 'uppercase',
+  },
+});
